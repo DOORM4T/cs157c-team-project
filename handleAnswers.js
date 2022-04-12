1e3aa9df95fd6bce5b921e47e01ac9268e270d94
Matthew Seto <46092255+DOORM4T@users.noreply.github.com>
2022-04-12 09:56:45 -0700

    Set up prompts

---
+++ handleAnswers.js
@@ -0,0 +1,15 @@
+async function handleAnswers(answers) {
+    console.log(answers)
+
+    // TODO: Query 1
+
+    // TODO: Query 2
+
+    // TODO: Query 3
+
+    // TODO: Query 4
+
+    // TODO: Query 5
+}
+
+module.exports = handleAnswers
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("hello world!")
+const prompt = require('./prompt')
+const handleAnswers = require('./handleAnswers')
+
+prompt().then(handleAnswers).catch((error) => {
+    console.error(error)
+    console.info("An unexpected error occurred. Exiting the program.")
+    process.exit(1)
+})
