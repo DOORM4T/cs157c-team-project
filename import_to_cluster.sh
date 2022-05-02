848fac0f556880ec6408c856e02e88a1330950ce
Matthew Seto <46092255+DOORM4T@users.noreply.github.com>
2022-05-01 17:30:14 -0700

    Create import script

---
+++ import_to_cluster.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Description:
+#   Import JSON to the sharded cluster via Node 1
+
+
+# Usage:
+# ./import_to_cluster.sh <data>.json
+
+DATA_PATH=$1
+
+echo "Attempting to import $1 to node_1..."
+
+echo "Copying file..."
+docker cp $DATA_PATH node_1:/home/to_import.json
+
+echo "Importing..."
+docker exec node_1 \
+    mongoimport --file /home/to_import.json --jsonArray --db project --collection openflight --port 27020
+
+echo "Success!"
